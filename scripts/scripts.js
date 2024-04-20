9af61d8164652ac0afaa0bd0b7b5a1a9baa82cb8
brysonallanfrancis <144868464+brysonallanfrancis@users.noreply.github.com>
2024-04-20 15:01:46 -0600

    Java hide/show btn

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,3 +16,9 @@ document.getElementById('mobile-menu').addEventListener('click', function() {
     var navList = document.querySelector('.nav-list');
     navList.classList.toggle('active');
   });
+
+// Hide Element
+function toggleForm() {
+  var form = document.getElementById('myForm');
+  form.classList.toggle('hidden'); // Toggle the 'hidden' class
+}
